1e69f2ff6f637d9eaf9183c870bac4e97e4333d7
Xiaoli Shen <alabebop@gmail.com>
2015-06-04 22:55:55 +0200

    http file server

--- 10_tcp_time_server/program.js
+++ 10_tcp_time_server/program.js
@@ -3,7 +3,7 @@ var strftime = require( 'strftime' )
 
 var server = net.createServer( function (socket) {
 
-	socket.end( strftime ('%F %H:%M') )
+	socket.end( strftime ('%F %H:%M') + '\n' )
 
 } )
 server.listen( +process.argv[2] )

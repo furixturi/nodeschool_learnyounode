6731414a2593701a3c4df689ddbe6cafd7746a2c
Xiaoli Shen <alabebop@gmail.com>
2015-06-05 10:25:22 +0200

    http json api server

--- 13_http_json_api_server/program.js
+++ 13_http_json_api_server/program.js
@@ -3,8 +3,31 @@ var http = require('http'),
 
 var parsetimeUrl = '/api/parsetime',	
 	unixtimeUrl = '/api/unixtime',
-	queryStr = 'iso';
+	queryKey = 'iso';
 
 http.createServer( function(req, res) {
+	
+	var parsedUrl = url.parse(req.url, true);
+
+	var route = parsedUrl.pathname,
+		queryVal = parsedUrl.query[queryKey];
+
+	var date = new Date(queryVal),
+		resJson = {};
+
+	if( route === parsetimeUrl ) {
+		resJson = {
+			"hour" : date.getHours(),
+			"minute" : date.getMinutes(),
+			"second" : date.getSeconds()
+		}
+	} else if ( route === unixtimeUrl ) {
+		resJson = {
+			"unixtime" : date.getTime()
+		}
+	}
+
+	res.writeHead(200, {'Content-Type': 'application/json'})
+	res.write(JSON.stringify(resJson))
 
 }).listen( Number(process.argv[2]) )
